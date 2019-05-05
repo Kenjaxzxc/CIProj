db7905d8b4af5711e20180f75adefb744c6a88e5
kenji <kenjaxzxc@gmail.com>
2019-05-05 21:56:49 +0800

    commit

--- project.sql
+++ project.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2018 at 10:02 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: May 05, 2019 at 03:56 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -40,14 +38,7 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`account_id`, `account_type`, `username`, `password`) VALUES
-(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(2, 'teacher', '123456', 'e10adc3949ba59abbe56e057f20f883e'),
-(3, 'teacher', '1234562', '6978915ee7dbc5aff1bc34b59e8c0fc5'),
-(4, 'student', '123456222', '6561c46b23d21e4a2de0267f558a4537'),
-(5, 'student', '12341234', 'ed2b1f468c5f915f3f1cf75d7068baae'),
-(6, 'student', '456456', 'b51e8dbebd4ba8a8f342190a4b9f08d7'),
-(7, 'student', '33', '182be0c5cdcd5072bb1864cdee4d3d6e'),
-(8, 'student', '3232', '12e086066892a311b752673a28583d3f');
+(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -61,13 +52,6 @@ CREATE TABLE `grades` (
   `student_id` int(11) DEFAULT NULL,
   `grade` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `grades`
---
-
-INSERT INTO `grades` (`grade_id`, `subject_id`, `student_id`, `grade`) VALUES
-(1, 1, 8, '4');
 
 -- --------------------------------------------------------
 
@@ -87,98 +71,8 @@ CREATE TABLE `logs` (
 --
 
 INSERT INTO `logs` (`log_id`, `action`, `date`, `account_id`) VALUES
-(1, 'Created section', '2018-10-07 18:54:33', 1),
-(2, 'Created section', '2018-10-07 18:54:41', 1),
-(3, 'Created subject', '2018-10-07 18:55:27', 1),
-(4, 'Created subject', '2018-10-07 18:55:33', 1),
-(5, 'Added teacher', '2018-10-07 19:00:42', 1),
-(6, 'Added teacher', '2018-10-07 19:27:27', 1),
-(7, 'Added student', '2018-10-07 19:37:37', 1),
-(8, 'test', '2018-10-07 20:47:01', 2),
-(9, 'test2', '2018-10-07 20:57:24', 4),
-(10, 'Logout', '2018-10-07 21:13:26', 1),
-(11, 'Login', '2018-10-07 21:40:07', 3),
-(12, 'Logout', '2018-10-07 21:41:06', 3),
-(13, 'Logout', '2018-10-07 21:43:13', NULL),
-(14, 'Login', '2018-10-07 21:43:59', 3),
-(15, 'Logout', '2018-10-07 21:44:08', 3),
-(16, 'Login', '2018-10-07 21:44:13', 3),
-(17, 'Logout', '2018-10-07 21:44:35', 3),
-(18, 'Login', '2018-10-07 21:44:37', 3),
-(19, 'Logout', '2018-10-07 21:44:46', 3),
-(20, 'Login', '2018-10-07 21:44:57', 4),
-(21, 'Logout', '2018-10-07 21:45:47', 4),
-(22, 'Login', '2018-10-07 21:45:50', 4),
-(23, 'Logout', '2018-10-07 21:48:12', 4),
-(24, 'Login', '2018-10-07 21:53:32', 4),
-(25, 'Login', '2018-10-07 21:53:42', 4),
-(26, 'Logout', '2018-10-07 21:53:48', 4),
-(27, 'Login', '2018-10-07 21:54:22', 4),
-(28, 'Logout', '2018-10-07 21:54:29', 4),
-(29, 'Login', '2018-10-07 21:54:41', 3),
-(30, 'Logout', '2018-10-07 21:54:49', 3),
-(31, 'Login', '2018-10-07 21:54:53', 1),
-(32, 'Logout', '2018-10-07 21:55:59', 1),
-(33, 'Login', '2018-10-07 21:56:09', 3),
-(34, 'Logout', '2018-10-07 21:56:26', 3),
-(35, 'Login', '2018-10-08 11:26:39', 1),
-(36, 'Logout', '2018-10-08 11:47:25', 1),
-(37, 'Login', '2018-10-08 11:48:59', 1),
-(38, 'Logout', '2018-10-08 11:54:03', 1),
-(39, 'Login', '2018-10-08 11:58:10', 2),
-(40, 'Logout', '2018-10-08 12:00:49', 2),
-(41, 'Login', '2018-10-08 12:00:56', 1),
-(42, 'Logout', '2018-10-08 12:05:11', 1),
-(43, 'Logout', '2018-10-08 12:17:52', NULL),
-(44, 'Logout', '2018-10-08 12:18:09', NULL),
-(45, 'Login', '2018-10-08 12:19:16', 1),
-(46, 'Login', '2018-10-08 12:19:29', 1),
-(47, 'Login', '2018-10-08 12:22:35', 1),
-(48, 'Login', '2018-10-08 12:23:43', 1),
-(49, 'Login', '2018-10-08 12:23:59', 1),
-(50, 'Login', '2018-10-08 12:24:11', 1),
-(51, 'Login', '2018-10-08 12:24:22', 1),
-(52, 'Login', '2018-10-08 12:24:34', 1),
-(53, 'Login', '2018-10-08 12:24:51', 1),
-(54, 'Login', '2018-10-08 12:26:24', 2),
-(55, 'Logout', '2018-10-08 12:33:57', 2),
-(56, 'Login', '2018-10-08 12:36:13', 4),
-(57, 'Logout', '2018-10-08 12:36:41', 4),
-(58, 'Login', '2018-10-08 12:37:33', 1),
-(59, 'Logout', '2018-10-08 12:37:41', 1),
-(60, 'Login', '2018-10-08 12:37:48', 2),
-(61, 'Logout', '2018-10-08 15:26:47', 2),
-(62, 'Login', '2018-10-08 15:27:52', 1),
-(63, 'Added student', '2018-10-08 15:28:42', 1),
-(64, 'Logout', '2018-10-08 15:28:48', 1),
-(65, 'Login', '2018-10-08 15:28:58', 2),
-(66, 'Logout', '2018-10-08 15:29:07', 2),
-(67, 'Login', '2018-10-08 15:29:24', 1),
-(68, 'Added student', '2018-10-08 15:29:47', 1),
-(69, 'Logout', '2018-10-08 15:29:53', 1),
-(70, 'Login', '2018-10-08 15:29:59', 2),
-(71, 'Logout', '2018-10-08 15:33:23', 2),
-(72, 'Login', '2018-10-08 15:33:33', 1),
-(73, 'Added student', '2018-10-08 15:33:51', 1),
-(74, 'Logout', '2018-10-08 15:33:55', 1),
-(75, 'Login', '2018-10-08 15:34:00', 2),
-(76, 'Logout', '2018-10-08 15:34:41', 2),
-(77, 'Login', '2018-10-08 15:34:45', 1),
-(78, 'Added student', '2018-10-08 15:35:02', 1),
-(79, 'Logout', '2018-10-08 15:35:07', 1),
-(80, 'Login', '2018-10-08 15:35:16', 2),
-(81, 'Logout', '2018-10-08 17:05:25', 2),
-(82, 'Login', '2018-10-08 17:05:40', 4),
-(83, 'Logout', '2018-10-08 17:12:15', 4),
-(84, 'Login', '2018-10-08 17:12:19', 8),
-(85, 'Logout', '2018-10-08 17:29:44', 8),
-(86, 'Login', '2018-10-08 17:30:03', 2),
-(87, 'Logout', '2018-10-08 17:51:51', 2),
-(88, 'Login', '2018-10-08 17:52:13', 6),
-(89, 'Login', '2018-10-11 10:45:57', 2),
-(90, 'Login', '2018-10-12 12:56:41', 2),
-(91, 'Logout', '2018-10-12 15:57:28', 2),
-(92, 'Login', '2018-10-12 15:57:38', 4);
+(153, 'Logout', '2019-05-05 21:43:13', 1),
+(154, 'Login', '2019-05-05 21:43:19', 1);
 
 -- --------------------------------------------------------
 
@@ -190,14 +84,6 @@ CREATE TABLE `sections` (
   `section_id` int(11) NOT NULL,
   `section_name` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `sections`
---
-
-INSERT INTO `sections` (`section_id`, `section_name`) VALUES
-(1, 'computer1'),
-(2, 'computer2');
 
 -- --------------------------------------------------------
 
@@ -218,13 +104,6 @@ CREATE TABLE `students` (
   `date` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`student_id`, `account_id`, `section_id`, `subject_id`, `fname`, `lname`, `age`, `gender`, `profile_pic`, `date`) VALUES
-(8, 8, 2, 1, 'qwer', 'qwe', 23, 'f', '89d6b56d3535629a8d5ff36a16ed876b.jpg', '2018-10-08 15:35:02');
-
 -- --------------------------------------------------------
 
 --
@@ -235,14 +114,6 @@ CREATE TABLE `subjects` (
   `subject_id` int(11) NOT NULL,
   `subject_name` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `subjects`
---
-
-INSERT INTO `subjects` (`subject_id`, `subject_name`) VALUES
-(1, 'PHP'),
-(2, 'Android');
 
 -- --------------------------------------------------------
 
@@ -261,14 +132,6 @@ CREATE TABLE `teachers` (
   `gender` varchar(45) DEFAULT NULL,
   `date` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `teachers`
---
-
-INSERT INTO `teachers` (`teacher_id`, `account_id`, `section_id`, `subject_id`, `fname`, `lname`, `age`, `gender`, `date`) VALUES
-(1, 2, 2, 2, 'EM', 'Cabua', 23, 'm', '2018-10-07 19:00:42'),
-(2, 3, 1, 1, 'Tin', 'Claro', 23, 'f', '2018-10-07 19:27:27');
 
 --
 -- Indexes for dumped tables
@@ -330,44 +193,37 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
-
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=155;
 --
 -- AUTO_INCREMENT for table `sections`
 --
 ALTER TABLE `sections`
-  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -396,7 +252,6 @@ ALTER TABLE `students`
 --
 ALTER TABLE `teachers`
   ADD CONSTRAINT `fk_teachers_accounts1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
